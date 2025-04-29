d22d9c71373a308dd484a610d6db07285ffc8981
Grayson-Orr <grayson.orr@op.ac.nz>
2025-04-30 01:12:25 +1200

    update

--- lecture-notes/07-helper-files/RoomNodeTypeList.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RoomNodeTypeList", menuName = "Scriptable Objects/Dungeon/Room Node Type List")]
-public class RoomNodeTypeList : ScriptableObject
-{
-    public List<RoomNodeType> list;
-}
